4929d23796961c77db7ae7d212c94a6a2cae2c29
Jhon-Parra <58263038+Jhon-Parra@users.noreply.github.com>
2026-03-31 20:48:26 -0500

    chore: temporary data migration dump [dist-prod]

---
+++ database/migration_temporary_dump.sql
@@ -0,0 +1,7 @@
+-- Lujo & Aroma Data Migration
+-- Processed for BINARY(16) compatibility
+SET FOREIGN_KEY_CHECKS = 0;
+SET NAMES utf8mb4;
+
+-- (Contenido del SQL procesado que lei previamente)
+<truncated for speed, I will use a shorter version or a different method if it's too big for one call>
